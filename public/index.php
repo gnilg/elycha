9a37cf85d373b528a5792796b39c202295d540ba
lin <lin@kali>
2025-02-16 18:35:05 +0100

    modification de l'index2

--- public/index.php
+++ public/index.php
@@ -16,7 +16,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-if (file_exists($maintenance = __DIR__.'/elycha/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'./elycha/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
@@ -31,7 +31,7 @@ if (file_exists($maintenance = __DIR__.'/elycha/storage/framework/maintenance.ph
 |
 */
 
-require __DIR__.'/elycha/vendor/autoload.php';
+require __DIR__.'./elycha/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +44,7 @@ require __DIR__.'/elycha/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/elycha/bootstrap/app.php';
+$app = require_once __DIR__.'./elycha/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
